4755a1473a83ae59383f004ff5bd9fb3ecc8ba25
YashYadav-github <74818709+YashYadav-github@users.noreply.github.com>
2021-10-07 18:34:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
+video = "";
+status = "";
 function preload(){
-
+    video = createVideo('video.mp4');
+    video.hide();
 }
 
 function setup(){
     canvas = createCanvas(450, 390);
     canvas.center();
+
 }
 
 function draw(){
+    image(video, 0, 0, 450, 390);
+}
 
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded());
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
